2f269d114381b3b212e25f33fb09c58cefa2d442
tatenami <116863311+tatenami@users.noreply.github.com>
2024-08-14 22:12:27 +0900

    Add files via upload

--- install.sh
+++ install.sh
@@ -6,4 +6,8 @@ sudo cp libps5.so /lib64
 sudo cp libps5.so /usr/lib/pad
 sudo cp libps5.so /lib/pad
 
+sudo install libps5.so /usr/local/lib
+sudo ldconfig
+sudo cp Pad.hpp PS5.hpp /usr/local/include
+
 rm ./libps5.so
